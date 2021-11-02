36dd8b147393df056d8683808c8c1bb5f0af601b
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2021-11-01 19:56:03 -0700

    Semicolon!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -101,7 +101,7 @@ public class Autonomous1 extends LinearOpMode {
             telemetry.addData("Pos", pos);
             Acceleration accel = bnimu.getAcceleration();
             Acceleration grav = bnimu.getGravity();
-            Acceleration floating = new Acceleration(DistanceUnit.MM, accel.xAccel - grav.xAccel, accel.yAccel - grav.yAccel, accel.zAccel - grav.zAccel, System.nanoTime())
+            Acceleration floating = new Acceleration(DistanceUnit.MM, accel.xAccel - grav.xAccel, accel.yAccel - grav.yAccel, accel.zAccel - grav.zAccel, System.nanoTime());
             telemetry.addData("Accel", floating);
             telemetry.addData("Status", "Running");
             telemetry.update();
