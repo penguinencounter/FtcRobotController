adf8d1c706aed0d508d65795288fc132f69330f6
Miles Lin <49845522+penguinencounter@users.noreply.github.com>
2022-01-31 19:00:46 -0800

    backup!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUtest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name = "IMU test", group = "Debug")
+public class IMUtest extends LinearOpMode {
+    IMUtools imutools;
+    @Override
+    public void runOpMode() {
+        Gyroscope imu = hardwareMap.get(Gyroscope.class, "imu");
+        imutools = new IMUtools(imu);
+        waitForStart();
+        while (opModeIsActive()) {
+            Orientation o = imutools.retrieveOrientation();
+            telemetry.addData("IMU Orientation", "1: %f.1 2: %f.1 3: %f.1",
+                    o.firstAngle, o.secondAngle, o.thirdAngle);
+            telemetry.update();
+        }
+    }
+}
