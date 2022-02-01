6ea923e593c1bcd3f585ef7a956f8e47dc75c192
Miles Lin <49845522+penguinencounter@users.noreply.github.com>
2022-01-31 20:19:43 -0800

    IMUd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous2.java
@@ -1,11 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gyroscope;
 
+@Autonomous(name="Auto r2 (testing)")
 public class Autonomous2 extends LinearOpMode {
 
+    IMUtools imutools;
     @Override
     public void runOpMode() {
-
+        Gyroscope gyro = hardwareMap.get(Gyroscope.class, "imu");
+        imutools = IMUtools.convert(gyro);
+        waitForStart();
+        while (opModeIsActive()) {
+            imutools.retrieveCacheOrientation();
+            imutools.wrapIt();
+            telemetry.addData("Heading", imutools.heading);
+            telemetry.update();
+        }
     }
 }
